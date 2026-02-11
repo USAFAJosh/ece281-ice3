fb89a6d73bc6723ea1e22a77a9fd0fa1850245d3
USAFAJosh <253311381+USAFAJosh@users.noreply.github.com>
2026-02-11 14:16:38 -0700

    Testing Done

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,7 +49,15 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "Cin" severity failure;
+	   w_addends <= x"88"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with Cout" severity failure;
+	   w_addends <= x"77"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"f" and w_Cout = '0') report "all but Cout" severity failure;
+	   w_addends <= x"67"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"e" and w_Cout = '0') report "bad with normal" severity failure;	   
+	   	   
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
